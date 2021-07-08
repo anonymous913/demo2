8bea1d979eefd32a214ecc846c5b4d6a3a003d59
anonymous913 <69008708+anonymous913@users.noreply.github.com>
2021-07-08 21:21:03 +0600

    f

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$project= $_POST['project'];
+$message= $_POST['message'];
+$to = "mahfuzurrahman.writter@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: noreply@demo2.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
